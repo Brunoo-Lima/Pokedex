fa23149d7dd822848c2c69fd9335378998caca1c
Bruno Lima <bruno2.jean.lima@outlook.com>
2023-07-25 16:04:54 -0300

    :tada: first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="bg-dark p-4">
+      <div className="container">
+        <a className="h2 link text-white" href="">
+          Pokedex
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
